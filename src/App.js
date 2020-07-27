c4915da54ce1e27053f1568b1371b80db4b1c54c
ellie fard <enesarifard@gmail.com>
2020-07-27 06:42:35 -0400

    added route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
-
+import Route from './components/Route';
 const items = [
   {
     title: 'What is React?',
@@ -34,24 +34,53 @@ const options = [
   },
 ];
 
+const showAccordian = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate />;
+  }
+};
 export default () => {
+  const [selected, setSelected] = useState();
   return (
     <div>
-      <Translate />
+      <Route path='/'>
+        <Accordion items={items} />
+      </Route>
+
+      <Route path='/list'>
+        <Search />
+      </Route>
+
+      <Route path='/dropdown'>
+        <Dropdown
+          label='Select a color'
+          options={options}
+          selected={selected}
+          onSeelectedChange={setSelected}
+        />
+      </Route>
+
+      <Route path='/translate'>
+        <Translate />
+      </Route>
     </div>
   );
 };
-
-// export default () => {
-//   const [selected, setSelected] = useState(options[0]);
-//   const [showDropdown, setShowDropdown] = useState(true);
-
-//   return (
-//     <div>
-//       <button onClick={() => setShowDropdown(!showDropdown)}> Toggle Dropdown</button>
-//       {showDropdown ? (
-//         <Dropdown selected={selected} onSelectedChange={setSelected} options={options} />
-//       ) : null}
-//     </div>
-//   );
-// };
