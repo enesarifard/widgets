16cb9955e944ea9da163e2715712e9e7a71192b9
ellie fard <enesarifard@gmail.com>
2020-07-27 07:01:10 -0400

    added header and link

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
 import Route from './components/Route';
+import Header from './components/Header';
+
 const items = [
   {
     title: 'What is React?',
-    content: 'React is a front end javascrypt framework',
+    content: 'React is a front end javascript framework',
   },
   {
-    title: 'why use React',
+    title: 'Why use React?',
     content: 'React is a favorite JS library among engineers',
   },
   {
@@ -21,63 +23,39 @@ const items = [
 
 const options = [
   {
-    label: 'The color red',
+    label: 'The Color Red',
     value: 'red',
   },
   {
-    label: 'The color green',
+    label: 'The Color Green',
     value: 'green',
   },
   {
-    label: 'Shade of blue',
+    label: 'A Shade of Blue',
     value: 'blue',
   },
 ];
 
-const showAccordian = () => {
-  if (window.location.pathname === '/') {
-    return <Accordion items={items} />;
-  }
-};
-
-const showList = () => {
-  if (window.location.pathname === '/list') {
-    return <Search />;
-  }
-};
-
-const showDropdown = () => {
-  if (window.location.pathname === '/dropdown') {
-    return <Dropdown />;
-  }
-};
-
-const showTranslate = () => {
-  if (window.location.pathname === '/translate') {
-    return <Translate />;
-  }
-};
 export default () => {
-  const [selected, setSelected] = useState();
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
+      <Header />
       <Route path='/'>
         <Accordion items={items} />
       </Route>
-
       <Route path='/list'>
         <Search />
       </Route>
-
       <Route path='/dropdown'>
         <Dropdown
           label='Select a color'
           options={options}
           selected={selected}
-          onSeelectedChange={setSelected}
+          onSelectedChange={setSelected}
         />
       </Route>
-
       <Route path='/translate'>
         <Translate />
       </Route>
